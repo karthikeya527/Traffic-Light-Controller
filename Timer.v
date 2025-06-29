369d746d192d3d1677b44180634f92e3f20d3a1f
karthikeya527 <karthikeya5270@gmail.com>
2025-06-29 21:24:46 +0530

    Update Timer.v

--- Timer.v
+++ Timer.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.06.2025 15:56:17
-// Design Name: 
-// Module Name: Timer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 module Timer(
     input [3:0] value,
     input oneHz_Enable,
