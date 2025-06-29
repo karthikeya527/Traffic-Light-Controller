b1a170c020430c705935fbffef58c8454a7dc754
karthikeya527 <karthikeya5270@gmail.com>
2025-06-29 21:25:07 +0530

    Update WR.v

--- WR.v
+++ WR.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.06.2025 22:50:53
-// Design Name: 
-// Module Name: WR
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 
