6d83abd3d890c6be94f57d1afc9c71eabaa23692
karthikeya527 <karthikeya5270@gmail.com>
2025-06-29 21:25:21 +0530

    Update synchronizer.v

--- synchronizer.v
+++ synchronizer.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.06.2025 14:22:42
-// Design Name: 
-// Module Name: synchronizer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 module synchronizer(
     input Reset,
